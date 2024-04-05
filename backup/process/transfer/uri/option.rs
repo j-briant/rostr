4d83b1a54e5b6563dabd45902452ec17d4d5d507
j-briant <j.briant@protonmail.com>
2024-04-05 20:02:49 +0200

    remove old files

--- backup/process/transfer/uri/option.rs
+++
@@ -1,8 +0,0 @@
-/// RDatasetoptions are GDAL Datasetoptions
-#[derive(Debug, Deserialize, PartialEq)]
-struct RDatasetOption {
-    pub open_flags: Vec<String>,
-    pub allowed_drivers: Vec<String>,
-    pub open_options: Vec<String>,
-    pub sibling_files: Vec<String>,
-}
